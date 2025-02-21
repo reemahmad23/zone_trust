45594986621efc98e8cb57f6b8c81b44f1632b83
reemahmad <reemmahmad40@gmail.com>
2025-02-21 18:07:39 +0200

    create the project structure

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+
+  static const TextStyle styleBold24 = TextStyle(
+      color: Color(0xFF1B4965),
+      fontSize: 24,
+      fontFamily: 'Alef',
+      fontWeight: FontWeight.w700, 
+  );
+
+  static const TextStyle styleSemiBold24 = TextStyle(
+      color: Color(0xFF1B4965),
+      fontSize: 24,
+      fontFamily: 'Alef',
+      fontWeight: FontWeight.w700, 
+  );
+
+  static const TextStyle styleBold = TextStyle(
+      color: Color(0xFF1B4965),
+      fontSize: 24,
+      fontFamily: 'Alef',
+      fontWeight: FontWeight.w700, 
+  );
+
+  static const TextStyle styleRegular16 = TextStyle(
+      color: Color(0xFF2B2B2B),
+      fontSize: 16,
+      fontFamily: 'ABeeZee',
+      fontWeight: FontWeight.w400, 
+  );
+
+  static const TextStyle styleRegular13 = TextStyle(
+      color: Color(0xFF656565),
+      fontSize: 13,
+      fontFamily: 'ABeeZee',
+      fontWeight: FontWeight.w400, 
+  );
+
+  
+
+  
+}
